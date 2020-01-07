0e5446ea592081096b2a1176a4457eb5f58df598
Jehoszafat Zimnowoda <jzimnowo@gmail.com>
2020-01-07 15:48:01 +0100

    Use express with openapi

---
+++ app.js
@@ -0,0 +1,33 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const openapi = require('express-openapi');
+const path = require('path');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+openapi.initialize({
+  apiDoc: fs.readFileSync(path.resolve(__dirname, 'api-doc.yml'), 'utf8'),
+  app: app,
+  paths: path.resolve(__dirname, 'api-routes'),
+  operations: {
+    getUser: function get(req, res) {
+      res.status(200).json({
+        id: req.params.id,
+        name: req.query.name,
+        age: req.query.age
+      });
+    }
+  }
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status).json(err);
+});
+
+module.exports = app;
+
+
+app.listen(8080);
