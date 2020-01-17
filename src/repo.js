cf35dc286f38ca93e1b8676d0ad546bb5129b8d4
Jehoszafat Zimnowoda <otomi-stack-api@redkubes.com>
2020-01-17 21:42:51 +0100

    Create git dir if not exists

--- src/repo.js
+++ src/repo.js
@@ -53,17 +53,23 @@ class Repo {
 
   async clone() {
     console.debug("Cloning repo: " + this.url + " to: " + this.path)
-    if (fs.existsSync(this.path)) {
-      console.warn("Repo path '" + this.path + "' already exists and is not an empty directory.")
-      return
+
+    const isRepo = await this.git.checkIsRepo()
+    if (!isRepo){
+      const remote = `https://${this.user}:${this.password}@${this.url}`;
+      await this.git.clone(remote, this.path)
+      await this.git.addConfig('user.name', this.user)
+      return await this.git.addConfig('user.email', this.email)
     }
 
-    const remote = `https://${this.user}:${this.password}@${this.url}`;
-    await this.git.clone(remote, this.path)
+    return isRepo
+
   }
 }
 
-module.exports = function (localPath, url, user, password) {
-  return new Repo(localPath, url, user, password)
+module.exports = function (localPath, url, user, email, password) {
+  if (!fs.existsSync(localPath))
+    fs.mkdirSync(localPath, 0o744);
+  return new Repo(localPath, url, user, email, password)
 }
 
