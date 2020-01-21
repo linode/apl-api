fd8d92b5eb667d0d61a8d1337027c2c30fa1b53c
Jehoszafat Zimnowoda <otomi-stack-api@redkubes.com>
2020-01-21 12:15:22 +0100

    Handle signals

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ const otomiStack = new otomi.OtomiStack(r, d)
 otomiStack.init().then(() => {
   const app = server.initApp(otomiStack)
   console.info("Listening on port: 8080")
-  app.listen(8080);
+  const srv = app.listen(8080);
+  utils.setSignalHandlers(srv)
 }
 )
 
