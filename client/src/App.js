740d198e731cc04bcdc966ef63ad2d6530f16bce
Jehoszafat Zimnowoda <otomi-stack-api@redkubes.com>
2020-02-07 13:02:54 +0100

    Added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
 import './App.css';
+
+import Home from './Home'
+import Service from './Service'
+import Services from './Services'
+import Team from './Team'
+import Teams from './Teams'
+
 
 function App() {
   return (
+    // <div className="App">
+
+    // </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/teams">Teams</Link>
+              </li>
+              <li>
+                <Link to="/teams/team1 ">Service</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/teams/:teamId/services/:serviceId">
+              <Service />
+            </Route>
+            <Route path="/teams/:teamId/services">
+              <Services />
+            </Route>
+            <Route path="/teams/:teamId">
+              <Team />
+            </Route>
+            <Route path="/teams/">
+              <Teams />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
