19113671f9c1dd556f66df36596445667911a3d5
Jehoszafat Zimnowoda <otomi-stack-api@redkubes.com>
2020-02-11 16:35:26 +0100

    Added tooltip

--- client/src/Service.js
+++ client/src/Service.js
@@ -1,15 +1,48 @@
 import React from "react";
 
 import Form from "react-jsonschema-form";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
+import { IoIosHelpCircle } from "react-icons/io";
 
 const log = (type) => console.log.bind(console, type);
 
 const uiSchema = {
-  serviceId: {"ui:widget": "hidden"},
-  teamId: {"ui:widget": "hidden"},
-  svc: {"ui:widget": "hidden"}
+  serviceId: { "ui:widget": "hidden" },
+  teamId: { "ui:widget": "hidden" },
+  svc: { "ui:widget": "hidden" }
 };
 
+
+
+const CustomDescriptionField = ({ id, description }) => {
+
+  function renderTooltip(props) {
+    return <Tooltip {...props}>{description}</Tooltip>;
+  }
+
+  function renderOverlay() {
+    return (
+    <OverlayTrigger
+      placement="right"
+      delay={{ show: 250, hide: 400 }}
+      overlay={renderTooltip}>
+      <IoIosHelpCircle />
+    </OverlayTrigger>
+    )
+  }
+  let desc = null
+  if(description)
+    desc = renderOverlay()
+
+  return (
+    desc
+  )
+};
+
+const fields = {
+  DescriptionField: CustomDescriptionField
+};
 
 class Service extends React.Component {
   state = { service: {} };
@@ -20,7 +53,7 @@ class Service extends React.Component {
 
   getData = () => {
     console.log(this.props.serviceId)
-    this.props.client.getServiceFromTeam({teamId: this.props.teamId, serviceId: this.props.serviceId}).then((response) => {
+    this.props.client.getServiceFromTeam({ teamId: this.props.teamId, serviceId: this.props.serviceId }).then((response) => {
       this.setState({ service: response.data })
     }).catch((error) => {
       console.log(error);
@@ -32,9 +65,9 @@ class Service extends React.Component {
 
     return (
       <div className="Service">
-        <h2>Service: {this.props.serviceId}</h2> 
+        <h2>Service: {this.props.serviceId}</h2>
 
-        <Form 
+        <Form
           schema={schema}
           uiSchema={uiSchema}
           disabled
@@ -66,6 +99,7 @@ class CreateService extends React.Component {
         <Form
           key='createService'
           schema={schema}
+          fields={fields}
           uiSchema={uiSchema}
           onChange={log("changed")}
           onSubmit={this.onSubmit}
