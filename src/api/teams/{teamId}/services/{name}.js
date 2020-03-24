c9f3761d27d2cd665a1b5f45b9b27902af6df2cc
Maurice Faber <morriz@idiotz.nl>
2020-03-24 20:56:10 +0100

    fixed put to service

--- src/api/teams/{teamId}/services/{name}.js
+++ src/api/teams/{teamId}/services/{name}.js
@@ -10,7 +10,7 @@ module.exports = function(otomi) {
     put: [
       function(req, res, next) {
         console.debug('Modify service: ' + JSON.stringify(req.params))
-        const data = otomi.editService(req.params.teamId, req.body)
+        const data = otomi.editService(req.params.teamId, req.params.name, req.body)
         res.status(200).json(data)
       },
     ],
