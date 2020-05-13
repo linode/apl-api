57d61a8151bec887e16b4731a847e69fe1566fd6
Maurice Faber <morriz@idiotz.nl>
2020-05-14 01:12:07 +0200

    fix: os dependend base64 decode [ci skip]

--- bin/get-team-kubeconf.sh
+++ bin/get-team-kubeconf.sh
@@ -6,7 +6,7 @@ OSX=false
 uname -a | grep Darwin >/dev/null
 [ $? -eq 0 ] && OSX=true
 
-$decode='-d'
+decode='-d'
 [ $OSX ] && decode='--decode'
 
 KUBE_VERSION=${KUBE_VERSION:-v1.15}
@@ -21,33 +21,33 @@ TARGET_FOLDER="/tmp/kube"
 [ -f $KUBECFG_FILE_NAME ] && exit
 
 create_target_folder() {
-  echo -n "Creating target directory to hold files in ${TARGET_FOLDER}..."
+  printf "Creating target directory to hold files in ${TARGET_FOLDER}..."
   mkdir -p "${TARGET_FOLDER}"
   printf "done"
 }
 
 get_secret_name_from_service_account() {
-  echo -e "\\nGetting secret of service account default on ${NAMESPACE}"
+  echo "Getting secret of service account default on ${NAMESPACE}"
   SECRET_NAME=$($kubectl get sa "default" --namespace="${NAMESPACE}" -o json | jq -r .secrets[].name)
   echo "Secret name: ${SECRET_NAME}"
 }
 
 extract_ca_crt_from_secret() {
-  echo -e -n "\\nExtracting ca.crt from secret..."
+  printf "\nExtracting ca.crt from secret..."
   $kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o json | jq \
     -r '.data["ca.crt"]' | base64 $decode >"${TARGET_FOLDER}/ca.crt"
   printf "done"
 }
 
 get_user_token_from_secret() {
-  echo -e -n "\\nGetting user token from secret..."
+  printf "\nGetting user token from secret..."
   USER_TOKEN=$($kubectl get secret --namespace "${NAMESPACE}" "${SECRET_NAME}" -o json | jq -r '.data["token"]' | base64 $decode)
   printf "done"
 }
 
 set_kube_config_values() {
   context=$($kubectl config current-context)
-  echo -e "\\nSetting current context to: $context"
+  printf "\nSetting current context to: $context"
 
   CLUSTER_NAME=$($kubectl config get-contexts "$context" | awk '{print $3}' | tail -n 1)
   echo "Cluster name: ${CLUSTER_NAME}"
@@ -57,8 +57,8 @@ set_kube_config_values() {
   echo "Endpoint: ${ENDPOINT}"
 
   # Set up the config
-  echo -e "\\nPreparing $KUBECFG_FILE_NAME"
-  echo -n "Setting a cluster entry in kubeconfig..."
+  echo "Preparing $KUBECFG_FILE_NAME"
+  echo "Setting a cluster entry in kubeconfig..."
   # $kubectl config set-cluster "${CLUSTER_NAME}" \
   # --kubeconfig="${KUBECFG_FILE_NAME}" \
   # --server="${ENDPOINT}" \
@@ -69,13 +69,13 @@ set_kube_config_values() {
     --server="${ENDPOINT}" \
     --insecure-skip-tls-verify
 
-  echo -n "Setting token credentials entry in kubeconfig..."
+  echo "Setting token credentials entry in kubeconfig..."
   $kubectl config set-credentials \
     "${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
     --token="${USER_TOKEN}"
 
-  echo -n "Setting a context entry in kubeconfig..."
+  echo "Setting a context entry in kubeconfig..."
   $kubectl config set-context \
     "${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}" \
@@ -83,7 +83,7 @@ set_kube_config_values() {
     --user="${NAMESPACE}-${CLUSTER_NAME}" \
     --namespace="${NAMESPACE}"
 
-  echo -n "Setting the current-context in the kubeconfig file..."
+  echo "Setting the current-context in the kubeconfig file..."
   $kubectl config use-context "${NAMESPACE}-${CLUSTER_NAME}" \
     --kubeconfig="${KUBECFG_FILE_NAME}"
 }
@@ -95,8 +95,7 @@ get_user_token_from_secret
 set_kube_config_values
 rm ${TARGET_FOLDER}/ca.crt
 
-echo -e "\\nAll done! Test with:"
-echo "KUBECONFIG=${KUBECFG_FILE_NAME} $kubectl get pods"
-echo "You will probably have nothing running yet, but if you see no errors you have access."
-echo ""
+printf "\nAll done! Test with:\n\n"
+printf "KUBECONFIG=${KUBECFG_FILE_NAME} $kubectl get pods\n"
+printf "You will probably have nothing running yet, but if you see no errors you have access.\n"
 echo "TOKEN: $USER_TOKEN"
