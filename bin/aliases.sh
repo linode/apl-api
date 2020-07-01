980726c0c879054f4db39b3d41fab31c649105a8
Maurice Faber <morriz@idiotz.nl>
2020-07-01 15:16:55 +0200

    improvement: added feedback in case team project exists [ci skip]

---
+++ bin/aliases.sh
@@ -0,0 +1,23 @@
+function drun() { $@; }
+function h() { helm $@; }
+
+alias d="docker"
+alias k="kubectl"
+alias ksk="k -n kube-system"
+alias ki="k -n ingress"
+alias kh="k -n harbor"
+alias kis="k -n istio-system"
+alias ks="k -n system"
+alias ksh="k -n shared"
+alias km="k -n monitoring"
+alias kta="k -n team-admin"
+alias ka="k --all-namespaces=true"
+alias kaa="ka get po,rs,job,deploy,ds,statefulset,svc"
+alias kap="ka get po"
+alias kdel="k delete"
+alias kcv="k config view"
+alias kce="$EDITOR ~/.kube/config"
+alias kcg="k config view | grep 'current-context:' | sed -n -e 's/^.*current-context: //p'"
+alias kcu="k config use-context"
+alias kp="k proxy &"
+alias kp="killall kubectl"
