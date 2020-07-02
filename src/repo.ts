382dce5f10b6ecd6d9b317423572585b33c790fb
Maurice Faber <morriz@idiotz.nl>
2020-07-02 18:21:33 +0200

    fix: writefile + .enc

--- src/repo.ts
+++ src/repo.ts
@@ -44,7 +44,7 @@ export class Repo {
   }
 
   writeFile(relativePath, data) {
-    const absolutePath = path.join(this.path, relativePath)
+    const absolutePath = path.join(this.path, relativePath) + '.dec'
     console.debug(`Writing to file: ${absolutePath}`)
     const yamlStr = yaml.safeDump(data)
     fs.writeFileSync(absolutePath, yamlStr, 'utf8')
