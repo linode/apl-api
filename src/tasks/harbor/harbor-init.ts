6b002f86d52efa0db2d111ab8111d6db640a93fe
Jehoszafat Zimnowoda <otomi-stack-api@redkubes.com>
2020-07-14 13:31:37 +0200

    feat: perform initial harbor configuration

--- src/tasks/harbor/harbor-init.ts
+++ src/tasks/harbor/harbor-init.ts
@@ -17,17 +17,16 @@ async function main() {
 
     const config: Configurations = {
       authMode: 'oidc_auth',
-      oidcVerifyCert: false,
-      oidcScope: 'openid',
-      oidcName: 'Azure',
-      oidcGroupsClaim: 'groups',
-      oidcClientId: '9c63a03a-40f8-4636-852b-f90d4b1bbbd0',
-      oidcEndpoint: 'https://login.microsoftonline.com/57a3f6ea-7e70-4260-acb4-e06ce452f695/v2.0',
+      oidcClientId: env.OIDC_CLIENT_ID,
+      oidcClientSecret: env.OIDC_CLIENT_SECRET,
+      oidcEndpoint: env.OIDC_ENDPOINT,
+      oidcGroupsClaim: env.OIDC_GROUPS_CLAIM,
+      oidcName: env.OIDC_NAME,
+      oidcScope: env.OIDC_SCOPE,
+      oidcVerifyCert: env.OIDC_VERIFY_CERT == 'true',
     }
-    const res = await api.configurationsPut(config)
+    await api.configurationsPut(config)
     console.info(`Successfully configured harbor`)
-
-    console.error(JSON.stringify(res))
   } catch (e) {
     console.error(JSON.stringify(e))
     process.exit(1)
