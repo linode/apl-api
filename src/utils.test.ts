f246d6b5ef974d5949ea292ff005d348d33080ec
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2021-03-18 10:54:41 +0100

    fix: remove test

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { removeBlankAttributes, getObjectPaths, getPublicUrl } from './utils'
+import { getObjectPaths, getPublicUrl } from './utils'
 
 describe('Utils', () => {
   it('should retrieve host part from service domain', (done) => {
@@ -36,39 +36,6 @@ describe('Utils', () => {
     }
     const paths = getObjectPaths(obj)
     expect(paths).to.have.members(['a', 'b.bb', 'c[0].ee.fff', 'c[1].ee.fff', 'd.dd[0]', 'd.dd[1]'])
-    done()
-  })
-
-  it('should clean blank object attributes', (done) => {
-    const obj = {
-      a: 1,
-      b: { bb: null },
-      c: [{ ca: { fff: undefined } }, { cb: { fff: 4 } }],
-      d: { dd: [1, 2] },
-      e: null,
-      f: {},
-      g: [],
-      h: '',
-      i: undefined,
-      j: { aa: {}, bb: 1 },
-      k: { aa: [], bb: 1 },
-      l: { aa: null, bb: 1 },
-      m: { aa: undefined, bb: 1 },
-      n: { aa: '', bb: 1 },
-    }
-
-    const expectedObj = {
-      a: 1,
-      c: [{ cb: { fff: 4 } }],
-      d: { dd: [1, 2] },
-      j: { bb: 1 },
-      k: { bb: 1 },
-      l: { bb: 1 },
-      m: { bb: 1 },
-      n: { bb: 1 },
-    }
-    const cleanedObject = removeBlankAttributes(obj)
-    expect(cleanedObject).to.deep.equal(expectedObj)
     done()
   })
 })
