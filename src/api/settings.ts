80c602f1a4b75e5679c1cf07914183d6a68aa16e
svatwork <sebastiaan.verbeek@redkubes.com>
2021-03-26 13:35:54 +0000

    fix: http 500 if object

--- src/api/settings.ts
+++ src/api/settings.ts
@@ -3,7 +3,7 @@ import OtomiStack from '../otomi-stack'
 import { OpenApiRequest } from '../otomi-models'
 
 export default function (otomi: OtomiStack) {
-  const GET: Operation = [(req: OpenApiRequest, res) => res.status(200).json(otomi.getSettings()[0])]
+  const GET: Operation = [(req: OpenApiRequest, res) => res.status(200).json(otomi.getSettings())]
   const PATCH: Operation = [
     (req: OpenApiRequest, res) => {
       otomi.editSettings(req.body)
