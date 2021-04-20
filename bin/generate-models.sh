92e1ca2a602ab9eee73cac249cba66eeee4ce696
svatwork <sebastiaan.verbeek@redkubes.com>
2021-04-20 14:46:59 +0200

    fix: models and build client otomi inline schema

--- bin/generate-models.sh
+++ bin/generate-models.sh
@@ -2,5 +2,6 @@
 set -e
 
 api_file=src/openapi.yaml
-npx openapi bundle --dereferenced --output src/openapi --ext yaml src/openapi/api.yaml
-npx openapi-typescript $api_file -o src/generated-schema.ts
+npx openapi bundle --output src/openapi --ext yaml src/openapi/api.yaml
+npx openapi bundle --dereferenced --output src/openapi-dereferenced --ext yaml src/openapi/api.yaml
+npx openapi-typescript src/openapi-dereferenced.yaml -o src/generated-schema.ts
