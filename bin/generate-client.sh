ebc319a6c7b00425ee3adb053883c0dae8f6da97
svatwork <sebastiaan.verbeek@redkubes.com>
2021-04-22 16:54:45 +0200

    refactor: generate client

--- bin/generate-client.sh
+++ bin/generate-client.sh
@@ -7,7 +7,7 @@ repo="ssh://git@github.com/redkubes/otomi-api.git"
 
 vendor="otomi-api"
 type="axios"
-openapi_doc="vendors/openapi/$vendor.json"
+openapi_doc="src/openapi.yaml"
 registry="https://npm.pkg.github.com/"
 target_dir="vendors/client/$vendor/$type"
 target_package_json="$target_dir/package.json"
@@ -29,6 +29,9 @@ validate() {
 generate_client() {
     echo "Generating client code from openapi specification $openapi_doc.."
     rm -rf $target_dir >/dev/null
+
+    npx openapi bundle --output src/openapi --ext yaml src/openapi/api.yaml
+
     docker run --rm -v $PWD:/local -w /local -u "$(id -u $USER)" \
     openapitools/openapi-generator-cli:v5.0.1 generate \
     -i /local/$openapi_doc \
--- bin/generate-models.sh
+++ bin/generate-models.sh
@@ -2,6 +2,5 @@
 set -e
 
 api_file=src/openapi.yaml
-npx openapi bundle --output src/openapi --ext yaml src/openapi/api.yaml
 npx openapi bundle --dereferenced --output src/openapi-dereferenced --ext yaml src/openapi/api.yaml
 npx openapi-typescript src/openapi-dereferenced.yaml -o src/generated-schema.ts
