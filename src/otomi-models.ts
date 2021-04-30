0e53f62eb3fd5318939103d87237550dff3b146a
svatwork <ajsverbeek@gmail.com>
2021-04-30 13:52:29 +0200

    revert: remove models from otomi-models

--- src/otomi-models.ts
+++ src/otomi-models.ts
@@ -3,17 +3,13 @@ import { components } from './generated-schema'
 
 export type Cloud = components['schemas']['Cloud']
 export type Cluster = components['schemas']['Cluster']
-export type Clusters = components['schemas']['Clusters']
 export type Deployment = components['schemas']['Deployment']
 export type Kubecfg = components['schemas']['Kubecfg']
 export type Secret = components['schemas']['Secret'] & { teamId?: string }
-export type Secrets = components['schemas']['Secrets']
 export type Service = components['schemas']['Service']
-export type Services = components['schemas']['Services']
 export type Session = components['schemas']['Session']
 export type Settings = components['schemas']['Settings']
 export type Team = components['schemas']['Team']
-export type Teams = components['schemas']['Teams']
 export type User = components['schemas']['User']
 
 export interface OpenApiRequest extends Request {
