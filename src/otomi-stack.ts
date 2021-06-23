eac55fb034001b344be7690a909b1b1d9fd670e4
Maurice Faber <maurice.faber@redkubes.com>
2021-06-23 17:05:27 +0200

    fix: kubecfg needed cluster input, fixes #180

--- src/otomi-stack.ts
+++ src/otomi-stack.ts
@@ -293,6 +293,7 @@ export default class OtomiStack {
     }
     const options = {
       users: [user],
+      clusters: [cluster],
       contexts: [context],
       currentContext: context.name,
     }
