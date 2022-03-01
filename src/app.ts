2610a901a7259e227dac1d5cfb6afe050b0a3199
otomi <not@us.ed>
2022-03-01 02:46:22 +0100

    fix: lint

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { createLightship } from 'lightship'
-import initApp from './server'
 import OtomiStack from './otomi-stack'
+import initApp from './server'
 
 console.log('NODE_ENV: ', process.env.NODE_ENV)
 
@@ -8,14 +8,10 @@ const otomiStack = new OtomiStack()
 otomiStack.init()
 
 async function initServer() {
-  let server
   const lightship = createLightship()
-  lightship.registerShutdownHandler(() => {
-    server.close()
-  })
   const app = await initApp(otomiStack)
   const { PORT = 8080 } = process.env
-  server = app
+  const server = app
     .listen(PORT, () => {
       console.info(`Listening on port: http://127.0.0.1:${PORT}`)
       lightship.signalReady()
@@ -23,6 +19,9 @@ async function initServer() {
     .on('error', () => {
       lightship.shutdown()
     })
+  lightship.registerShutdownHandler(() => {
+    server.close()
+  })
 }
 
 initServer().catch((e) => {
