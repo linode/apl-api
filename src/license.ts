cc775adfc41baa440582919e4bf69a4b0b8ce7a4
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2023-03-17 14:43:27 +0100

    feat: set default license paths

--- src/license.ts
+++ src/license.ts
@@ -1,15 +1,20 @@
+import path from 'path'
+
+export const defaultLicensePath = 'env/license.yaml'
+export const defaultPublicKeyPath = path.resolve(__dirname, 'license/license.pem')
+
 export function isLicenseValid(license: any): boolean {
   return false
 }
 
-export function loadLicense(path: string): Record<string, any> {
+export function loadLicense(filePath: string): Record<string, any> {
   return {}
 }
 
-export function loadPublicKey(path: string): string {
+export function loadPublicKey(filePath: string): string {
   return ''
 }
 
-export function saveLicense(path: string): string {
+export function saveLicense(filePath: string): string {
   return ''
 }
