c38db4c2befb2911d6a22d022ca3650fa9f30e68
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2023-03-17 14:34:46 +0100

    feat: add empty license module

---
+++ src/license.ts
@@ -0,0 +1,15 @@
+export function isLicenseValid(license: any): boolean {
+  return false
+}
+
+export function loadLicense(path: string): Record<string, any> {
+  return {}
+}
+
+export function loadPublicKey(path: string): string {
+  return ''
+}
+
+export function saveLicense(path: string): string {
+  return ''
+}
