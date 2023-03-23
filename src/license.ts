2e0c1d830a43f392b13ea56eee895e467f13749d
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2023-03-23 15:28:25 +0100

    feat: sign license

--- src/license.ts
+++ src/license.ts
@@ -1,20 +1,40 @@
+import Debug from 'debug'
+import { cleanEnv, str } from 'envalid'
+import { readFile, writeFile } from 'fs-extra'
+import jwt from 'jsonwebtoken'
 import path from 'path'
+import { parse as parseYaml } from 'yaml'
+import { License } from './otomi-models'
 
 export const defaultLicensePath = 'env/license.yaml'
 export const defaultPublicKeyPath = path.resolve(__dirname, 'license/license.pem')
 
-export function isLicenseValid(license: any): boolean {
-  return false
+const d = Debug('otomi:license')
+
+export async function cliSignLicense() {
+  const env = cleanEnv(process.env, {
+    SECRET_RSA_KEY_PATH: str({ desc: 'A private RSA key used for signing the license JWT' }),
+    PUBLIC_RSA_KEY_PATH: str({ desc: 'A public RSA key used for veryfing the signed license' }),
+    LICENSE_SRC_PATH: str({ desc: 'A license file in YAML format' }),
+    LICENSE_DST_PATH: str({ desc: 'Destination path of the signed license' }),
+  })
+
+  d(`SECRET_RSA_KEY_PATH: ${env.SECRET_RSA_KEY_PATH}`)
+  d(`PUBLIC_RSA_KEY_PATH: ${env.SECRET_RSA_KEY_PATH}`)
+  d(`LICENSE_SRC_PATH: ${env.LICENSE_SRC_PATH}`)
+  d(`LICENSE_DST_PATH: ${env.LICENSE_DST_PATH}`)
+
+  const license = parseYaml(await readFile(env.LICENSE_SRC_PATH, 'utf8')) as License['body']
+  const privateKey = await readFile(env.SECRET_RSA_KEY_PATH, 'utf8')
+  const publicKey = await readFile(env.PUBLIC_RSA_KEY_PATH, 'utf8')
+  const signedLicense = jwt.sign(license as object, privateKey, { algorithm: 'RS256' })
+  d(`Signed license has been save in the ${env.LICENSE_DST_PATH} file`)
+
+  d(`Veryfing signed license with public key`)
+  jwt.verify(signedLicense, publicKey, { algorithms: ['RS256'] })
+  d(`License has been succesfullly verified`)
+
+  writeFile(env.LICENSE_DST_PATH, signedLicense)
 }
 
-export function loadLicense(filePath: string): Record<string, any> {
-  return {}
-}
-
-export function loadPublicKey(filePath: string): string {
-  return ''
-}
-
-export function saveLicense(filePath: string): string {
-  return ''
-}
+cliSignLicense()
