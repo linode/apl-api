a161ee7365297fb97f5e0f0e01cdc8f87c0fb538
Jehoszafat Zimnowoda <jehoszafat.zimnowoda@redkubes.com>
2023-03-25 23:02:06 +0100

    feat: get rsa keys from env

--- src/license.ts
+++ src/license.ts
@@ -13,22 +13,20 @@ const d = Debug('otomi:license')
 
 export async function cliSignLicense() {
   const env = cleanEnv(process.env, {
-    SECRET_RSA_KEY_PATH: str({ desc: 'A private RSA key used for signing the license JWT' }),
-    PUBLIC_RSA_KEY_PATH: str({ desc: 'A public RSA key used for veryfing the signed license' }),
+    RSA_PRIVATE_KEY: str({ desc: 'A private RSA key used for signing the license JWT' }),
+    RSA_PUBLIC_KEY: str({ desc: 'A public RSA key used for veryfing the signed license' }),
     LICENSE_SRC_PATH: str({ desc: 'A license file in YAML format' }),
     LICENSE_DST_PATH: str({ desc: 'Destination path of the signed license' }),
   })
 
-  d(`SECRET_RSA_KEY_PATH: ${env.SECRET_RSA_KEY_PATH}`)
-  d(`PUBLIC_RSA_KEY_PATH: ${env.SECRET_RSA_KEY_PATH}`)
   d(`LICENSE_SRC_PATH: ${env.LICENSE_SRC_PATH}`)
   d(`LICENSE_DST_PATH: ${env.LICENSE_DST_PATH}`)
 
   const license = parseYaml(await readFile(env.LICENSE_SRC_PATH, 'utf8')) as License['body']
-  const privateKey = await readFile(env.SECRET_RSA_KEY_PATH, 'utf8')
-  const publicKey = await readFile(env.PUBLIC_RSA_KEY_PATH, 'utf8')
+  const privateKey = env.RSA_PRIVATE_KEY
+  const publicKey = env.RSA_PUBLIC_KEY
   const signedLicense = jwt.sign(license as object, privateKey, { algorithm: 'RS256' })
-  d(`Signed license has been save in the ${env.LICENSE_DST_PATH} file`)
+  d(`Signed license has been saved in the ${env.LICENSE_DST_PATH} file`)
 
   d(`Veryfing signed license with public key`)
   jwt.verify(signedLicense, publicKey, { algorithms: ['RS256'] })
