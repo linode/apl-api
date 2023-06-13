914f2dc0ec1da8a0d219d6fd8730fff5ad0c2f58
Ferruh Cihan <ferruh.cihan@redkubes.com>
2023-06-13 11:45:16 +0200

    feat: update watch

--- src/apply.ts
+++ src/apply.ts
@@ -48,46 +48,51 @@ export async function apply(specPath: string): Promise<k8s.KubernetesObject[]> {
 
   const watch = new k8s.Watch(kc)
 
-  await watch
-    .watch(
-      '/api/v1/pods',
-      // optional query parameters can go here.
-      {
-        allowWatchBookmarks: true,
-      },
-      // callback is called for each received object.
-      (type, apiObj, watchObj) => {
-        if (type === 'ADDED') {
+  try {
+    await watch
+      .watch(
+        '/api/v1/pods',
+        // optional query parameters can go here.
+        {
+          allowWatchBookmarks: true,
+        },
+        // callback is called for each received object.
+        (type, apiObj, watchObj) => {
+          if (type === 'ADDED') {
+            // tslint:disable-next-line:no-console
+            console.log('new object:')
+          } else if (type === 'MODIFIED') {
+            // tslint:disable-next-line:no-console
+            console.log('changed object:')
+          } else if (type === 'DELETED') {
+            // tslint:disable-next-line:no-console
+            console.log('deleted object:')
+          } else if (type === 'BOOKMARK') {
+            // tslint:disable-next-line:no-console
+            console.log(`bookmark: ${watchObj.metadata.resourceVersion}`)
+          } else {
+            // tslint:disable-next-line:no-console
+            console.log(`unknown type: ${type}`)
+          }
           // tslint:disable-next-line:no-console
-          console.log('new object:')
-        } else if (type === 'MODIFIED') {
+          console.log(apiObj)
+        },
+        // done callback is called if the watch terminates normally
+        (err) => {
           // tslint:disable-next-line:no-console
-          console.log('changed object:')
-        } else if (type === 'DELETED') {
-          // tslint:disable-next-line:no-console
-          console.log('deleted object:')
-        } else if (type === 'BOOKMARK') {
-          // tslint:disable-next-line:no-console
-          console.log(`bookmark: ${watchObj.metadata.resourceVersion}`)
-        } else {
-          // tslint:disable-next-line:no-console
-          console.log(`unknown type: ${type}`)
-        }
-        // tslint:disable-next-line:no-console
-        console.log(apiObj)
-      },
-      // done callback is called if the watch terminates normally
-      (err) => {
-        // tslint:disable-next-line:no-console
-        console.log(err)
-      },
-    )
-    .then((req) => {
-      // watch returns a request object which you can use to abort the watch.
-      setTimeout(() => {
-        req.abort()
-      }, 10 * 1000)
-    })
+          console.log(err)
+        },
+      )
+      .then((req) => {
+        console.log('req', req)
+        // watch returns a request object which you can use to abort the watch.
+        setTimeout(() => {
+          req.abort()
+        }, 10 * 1000)
+      })
+  } catch (error) {
+    console.log('watch error', error)
+  }
 
   return created
 }
