18bc8d009cf4ab71c0bdade3a82619a63ed39a5e
Ferruh Cihan <ferruh.cihan@redkubes.com>
2023-06-16 16:52:11 +0200

    fix: cloudtty

--- src/otomi-stack.ts
+++ src/otomi-stack.ts
@@ -682,12 +682,12 @@ export default class OtomiStack {
     await writeFile(`/tmp/ttyd-${data.teamId}.yaml`, fileContents, 'utf-8')
 
     await apply(`/tmp/ttyd-${data.teamId}.yaml`)
-    await watchPodUntilRunning('team-admin', `tty-${data.emailNoSymbols}-${data.teamId}}`)
+    await watchPodUntilRunning('team-admin', `tty-${data.emailNoSymbols}-${data.teamId}`)
     await wait3Seconds()
 
     return this.db.createItem(
       'cloudttys',
-      { ...data, iFrameUrl: `https://tty.${data.domain}/${data.emailNoSymbols}-${data.teamId}}` },
+      { ...data, iFrameUrl: `https://tty.${data.domain}/${data.emailNoSymbols}-${data.teamId}` },
       { teamId: data.teamId, name: `tty-${data.emailNoSymbols}-${data.teamId}` },
     ) as Cloudtty
   }
