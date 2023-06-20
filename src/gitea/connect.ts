229b3f15ef78dd572650ccdb095bbc315b472ffc
Matthew Elderhorst <matthew.elderhorst@redkubes.com>
2023-06-20 16:39:51 +0200

    fix: hardcoded domain

--- src/gitea/connect.ts
+++ src/gitea/connect.ts
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
 export default async function giteaCheckLatest(token: string, clusterData: any): Promise<string | void> {
-  const domainSuffix: string | undefined = clusterData?.cluster?.domainSuffix
+  let domainSuffix: string | undefined = clusterData?.cluster?.domainSuffix
   // let domainSuffix: string = os.networkInterfaces().lo0![0].address
   // if (env.NODE_ENV === 'development') domainSuffix = ''
   console.log('domain', domainSuffix)
+  domainSuffix = '161.35.245.248'
   if (domainSuffix) {
     await axios({
       url: `https://gitea.${domainSuffix}.nip.io/api/v1/repos/otomi/values`,
