d0c98cb91ef78a038028030e8da12bfd8e2503ad
Matthew Elderhorst <matthew.elderhorst@redkubes.com>
2023-06-21 10:58:04 +0200

    fix: change url to get commits of repo

--- src/gitea/connect.ts
+++ src/gitea/connect.ts
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
 export default async function giteaCheckLatest(token: string, clusterData: any): Promise<string | void> {
-  let domainSuffix: string | undefined = clusterData?.cluster?.domainSuffix
+  const domainSuffix: string | undefined = clusterData?.cluster?.domainSuffix
   // let domainSuffix: string = os.networkInterfaces().lo0![0].address
   // if (env.NODE_ENV === 'development') domainSuffix = ''
-  if (!domainSuffix) {
-    console.log('Domain suffix was empty, hardcoding it')
-    domainSuffix = '161.35.245.248.nip.io'
-  }
+  //   if (!domainSuffix) {
+  //     console.log('Domain suffix was empty, hardcoding it')
+  //     domainSuffix = '161.35.245.248.nip.io'
+  //   }
   console.log('domain', domainSuffix)
   if (domainSuffix) {
     await axios({
-      url: `https://gitea.${domainSuffix}/api/v1/repos/otomi/values`,
+      url: `https://gitea.${domainSuffix}/api/v1/repos/otomi/values/commits`,
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
