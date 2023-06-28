012949fceb52a86605179b1af2102bc54ed6c96c
Ferruh <63190600+ferruhcihan@users.noreply.github.com>
2023-06-28 17:18:59 +0200

    feat: cloudtty shell (#422)
    Co-authored-by: Ani Argjiri <ani.argjiri@redkubes.com>
    Co-authored-by: Dennis van Kekem <38350840+dennisvankekem@users.noreply.github.com>

---
+++ src/k8s_operations.ts
@@ -0,0 +1,119 @@
+import * as k8s from '@kubernetes/client-node'
+import Debug from 'debug'
+import * as fs from 'fs'
+import * as yaml from 'js-yaml'
+import { promisify } from 'util'
+import { Cloudtty } from './otomi-models'
+
+const debug = Debug('otomi:api:cloudtty')
+
+/**
+ * Replicate the functionality of `kubectl apply`.  That is, create the resources defined in the `specFile` if they do
+ * not exist, patch them if they do exist.
+ *
+ * @param specPath File system path to a YAML Kubernetes spec.
+ * @return Array of resources created
+ */
+export async function apply(specPath: string): Promise<k8s.KubernetesObject[]> {
+  const kc = new k8s.KubeConfig()
+  kc.loadFromDefault()
+  const client = k8s.KubernetesObjectApi.makeApiClient(kc) as any
+  const fsReadFileP = promisify(fs.readFile)
+  const specString = await fsReadFileP(specPath, 'utf8')
+  const specs: any = yaml.loadAll(specString)
+  const validSpecs = specs.filter((s) => s && s.kind && s.metadata)
+  const created: k8s.KubernetesObject[] = []
+  for (const spec of validSpecs) {
+    // this is to convince the old version of TypeScript that metadata exists even though we already filtered specs
+    // without metadata out
+    spec.metadata = spec.metadata || {}
+    spec.metadata.annotations = spec.metadata.annotations || {}
+    delete spec.metadata.annotations['kubectl.kubernetes.io/last-applied-configuration']
+    spec.metadata.annotations['kubectl.kubernetes.io/last-applied-configuration'] = JSON.stringify(spec)
+    try {
+      // try to get the resource, if it does not exist an error will be thrown and we will end up in the catch
+      // block.
+      await client.read(spec)
+      // we got the resource, so it exists, so patch it
+      //
+      // Note that this could fail if the spec refers to a custom resource. For custom resources you may need
+      // to specify a different patch merge strategy in the content-type header.
+      //
+      // See: https://github.com/kubernetes/kubernetes/issues/97423
+      const response = await client.patch(spec)
+      created.push(response.body)
+    } catch (e) {
+      // we did not get the resource, so it does not exist, so create it
+      const response = await client.create(spec)
+      created.push(response.body)
+    }
+  }
+  debug(`Cloudtty is created!`)
+  return created
+}
+
+export async function watchPodUntilRunning(namespace: string, podName: string) {
+  let isRunning = false
+  const kc = new k8s.KubeConfig()
+  kc.loadFromDefault()
+  const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
+
+  while (!isRunning) {
+    try {
+      const res = await k8sApi.readNamespacedPodStatus(podName, namespace)
+      isRunning = res.body.status?.phase === 'Running'
+    } catch (error) {
+      debug('watchPodUntilRunning error:', error)
+    }
+
+    if (!isRunning) {
+      debug(`Pod ${podName} is not running. Checking again in 5 seconds...`)
+      await new Promise((resolve) => setTimeout(resolve, 5000))
+    }
+  }
+  // wait 3 more seconds to make sure the pod is fully initialized
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+  debug(`Pod ${podName} is now running!`)
+  return true
+}
+
+export async function checkPodExists(namespace: string, podName: string) {
+  let isRunning = false
+  const kc = new k8s.KubeConfig()
+  kc.loadFromDefault()
+  const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
+
+  try {
+    const res = await k8sApi.readNamespacedPodStatus(podName, namespace)
+    isRunning = res.body.status?.phase === 'Running'
+  } catch (error) {
+    debug('checkPodExist error:', error)
+  }
+
+  return isRunning
+}
+
+export async function k8sdelete({ emailNoSymbols, isAdmin, userTeams }: Cloudtty) {
+  const kc = new k8s.KubeConfig()
+  kc.loadFromDefault()
+  const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
+  const customObjectsApi = kc.makeApiClient(k8s.CustomObjectsApi)
+  const rbacAuthorizationV1Api = kc.makeApiClient(k8s.RbacAuthorizationV1Api)
+  const resourceName = emailNoSymbols
+  const namespace = 'team-admin'
+  try {
+    await k8sApi.deleteNamespacedServiceAccount(`tty-${resourceName}`, namespace)
+    await k8sApi.deleteNamespacedPod(`tty-${resourceName}`, namespace)
+    if (!isAdmin) {
+      for (const team of userTeams!)
+        await rbacAuthorizationV1Api.deleteNamespacedRoleBinding(`tty-${team}-rolebinding`, team)
+    } else await rbacAuthorizationV1Api.deleteClusterRoleBinding('tty-admin-rolebinding')
+    await k8sApi.deleteNamespacedService(`tty-${resourceName}`, namespace)
+    const apiGroup = 'networking.istio.io'
+    const apiVersion = 'v1beta1'
+    const plural = 'virtualservices'
+    await customObjectsApi.deleteNamespacedCustomObject(apiGroup, apiVersion, namespace, plural, `tty-${resourceName}`)
+  } catch (error) {
+    debug('k8sdelete error:', error)
+  }
+}
