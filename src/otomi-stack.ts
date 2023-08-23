d93eddba688ca4da34d3e476e8030f31a6e0c0b3
Ferruh Cihan <ferruh.cihan@redkubes.com>
2023-08-23 18:45:06 +0200

    test: k8sVersion

--- src/otomi-stack.ts
+++ src/otomi-stack.ts
@@ -50,7 +50,7 @@ import {
   cleanEnv,
 } from 'src/validators'
 import { parse as parseYaml, stringify as stringifyYaml } from 'yaml'
-import { apply, checkPodExists, k8sdelete, watchPodUntilRunning } from './k8s_operations'
+import { apply, checkPodExists, getKubernetesVersion, k8sdelete, watchPodUntilRunning } from './k8s_operations'
 import connect from './otomiCloud/connect'
 
 const debug = Debug('otomi:otomi-stack')
@@ -650,6 +650,11 @@ export default class OtomiStack {
       }
     })
     return this.db.deleteItem('builds', { id })
+  }
+
+  async getK8sVersion(): Promise<string> {
+    const version = (await getKubernetesVersion()) as string
+    return version
   }
 
   async connectCloudtty(data: Cloudtty): Promise<Cloudtty | any> {
