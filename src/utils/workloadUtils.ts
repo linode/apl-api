963f40b27a8b5605047a545bc42d26a212965235
ferruh2021@gmail.com <ferruh.cihan@redkubes.com>
2023-09-29 13:02:53 +0200

    revert: workloadUtils

--- src/utils/workloadUtils.ts
+++ src/utils/workloadUtils.ts
@@ -12,41 +12,30 @@ function throwChartError(message: string) {
 
 export async function getWorkloadChart(
   revision: string,
-  giturl: string,
+  url: string,
   path: string,
   workloadName: string,
   teamId: string,
   emailNoSymbols: string,
 ): Promise<Promise<any>> {
   let shellResult
-  shellResult = shell.pwd()
-  console.log('shellResult', shellResult)
   const helmChartsDir = `/tmp/otomi/charts/${emailNoSymbols}/${teamId}-${workloadName}`
   shell.rm('-rf', helmChartsDir)
   shell.mkdir('-p', helmChartsDir)
-  // shell.cd(helmChartsDir)
-  let url = giturl
-
-  if (giturl.includes('gitea')) {
-    const [, giteaUrl] = giturl.split('://')
-    url = `https://${process.env.GIT_USER}:${process.env.GIT_PASSWORD}@${giteaUrl}`
-  }
+  shell.cd(helmChartsDir)
 
   if (revision !== 'HEAD') {
     const commitIDRegex = /^[0-9a-fA-F]{40}$/
     const isCommitID = commitIDRegex.test(revision)
 
     if (isCommitID) {
-      shell.exec(`git clone ${url} ${helmChartsDir}`)
+      shell.exec(`git clone ${url} .`)
       shellResult = shell.exec(`git reset --hard ${revision}`)
-    } else shellResult = shell.exec(`git clone --depth 1 --branch ${revision} ${url} ${helmChartsDir}`)
+    } else shellResult = shell.exec(`git clone --depth 1 --branch ${revision} ${url} .`)
 
     if (shellResult.code !== 0)
-      throwChartError(`Not found ${isCommitID ? 'commit' : 'branch or tag'} '${revision}' in '${giturl}'`)
-  } else shell.exec(`git clone --depth 1 ${url} ${helmChartsDir}`)
-
-  shellResult = shell.pwd()
-  console.log('shellResult', shellResult)
+      throwChartError(`Not found ${isCommitID ? 'commit' : 'branch or tag'} '${revision}' in '${url}'`)
+  } else shell.exec(`git clone --depth 1 ${url} .`)
 
   try {
     const v = await readFile(`${helmChartsDir}${path}/values.yaml`, 'utf-8')
@@ -59,6 +48,6 @@ export async function getWorkloadChart(
       chartDescription: customChart.description,
     }
   } catch (error) {
-    throwChartError(`There is no chart in '${giturl}' ${path ? ` path '${path}'` : ''}`)
+    throwChartError(`There is no chart in '${url}' ${path ? ` path '${path}'` : ''}`)
   }
 }
