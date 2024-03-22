e68759aa8543e07b0f0b6ed5dc39507560303768
ferruhcihan <ferruh.cihan@redkubes.com>
2024-03-22 15:30:48 +0100

    test: tekton message

--- src/otomi-stack.ts
+++ src/otomi-stack.ts
@@ -970,10 +970,10 @@ export default class OtomiStack {
         })
       }, 5 * 1000)
 
-      // fallback to clear interval after 10 minutes
-      setTimeout(() => {
-        clearInterval(intervalId)
-      }, 10 * 60 * 1000)
+      // // fallback to clear interval after 10 minutes
+      // setTimeout(() => {
+      //   clearInterval(intervalId)
+      // }, 10 * 60 * 1000)
     } catch (error) {
       debug('Error emitting pipeline status:', error)
     }
