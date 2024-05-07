77853ccdedbf3efaf70946ac6343c6841e864982
ferruhcihan <ferruh.cihan@redkubes.com>
2024-05-07 17:01:33 +0200

    feat: update create policies with customValues

--- src/utils/policiesUtils.ts
+++ src/utils/policiesUtils.ts
@@ -1,11 +1,47 @@
 import schema from '../generated-schema.json'
 
+function getCustomValues(policyName: string) {
+  if (policyName === 'disallow-capabilities') {
+    return [
+      'AUDIT_WRITE',
+      'CHOWN',
+      'DAC_OVERRIDE',
+      'FOWNER',
+      'FSETID',
+      'KILL',
+      'MKNOD',
+      'NET_BIND_SERVICE',
+      'SETFCAP',
+      'SETGID',
+      'SETPCAP',
+      'SETUID',
+      'SYS_CHROOT',
+      "''",
+    ]
+  }
+  if (policyName === 'restrict-volume-types') {
+    return [
+      'name',
+      'configMap',
+      'csi',
+      'downwardAPI',
+      'emptyDir',
+      'ephemeral',
+      'persistentVolumeClaim',
+      'projected',
+      'secret',
+      "''",
+    ]
+  }
+  return undefined
+}
+
 export function getPolicies() {
   const policies = schema.components.schemas.Policies.properties
-  return Object.entries(policies).reduce((acc: Record<string, any>, [name, policy]: any) => {
+  return Object.entries(policies).reduce((acc: Record<string, any>, [name, policy]) => {
     const action = policy.properties.action.default
     const severity = policy.properties.severity.default
-    const customValues = policy.properties.customValues ? [] : undefined
+    const customValues = getCustomValues(name)
     const newAcc = { ...acc, [name]: { action, severity, customValues } }
     return newAcc
   }, {})
