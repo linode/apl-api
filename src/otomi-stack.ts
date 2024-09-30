06ca47905681b0e94d9411baae2f38aada85f4be
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2024-09-30 12:53:31 +0200

    fix: saveTeamUsers

--- src/otomi-stack.ts
+++ src/otomi-stack.ts
@@ -1604,10 +1604,13 @@ export default class OtomiStack {
       const secretExists = await this.repo.fileExists(relativePath)
       if (!secretExists) secretRelativePath = relativePath
     }
-    if (cleaneUsers.length === 0) await this.repo.removeFile(relativePath)
     const outData: Record<string, any> = set({}, getTeamUsersJsonPath(teamId), cleaneUsers)
     debug(`Saving users of team: ${teamId}`)
     await this.repo.writeFile(secretRelativePath, outData)
+    if (cleaneUsers.length === 0) {
+      await this.repo.removeFile(relativePath)
+      await this.repo.removeFile(secretRelativePath)
+    }
   }
 
   async saveTeamProjects(teamId: string): Promise<void> {
