a7b6086ef34c354720b4ae6396f3749afb36d624
ElderMatt <18527012+ElderMatt@users.noreply.github.com>
2024-12-02 09:35:16 +0100

    fix: removed debug

--- src/api/objwizard.ts
+++ src/api/objwizard.ts
@@ -10,7 +10,6 @@ export default function (): OperationHandlerArray {
       debug('createObjWizard')
       const v = await otomi.createObjWizard(body as ObjWizard)
       res.json(v)
-      debug('Returned Value', v)
     },
   ]
   const api = {
