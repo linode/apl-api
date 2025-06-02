d0562bbe9366e95518d7bc96c83bdc5e5bb17adc
otomi-admin <63190600+ferruhcihan@users.noreply.github.com>
2025-06-02 09:17:33 +0200

    feat: new sealed secrets schema

--- src/utils/sealedSecretUtils.ts
+++ src/utils/sealedSecretUtils.ts
@@ -1,69 +1,8 @@
-import crypto, { X509Certificate } from 'crypto'
+import { X509Certificate } from 'crypto'
 import { isEmpty } from 'lodash'
 import { AplSecretResponse } from 'src/otomi-models'
-import { getSealedSecretsCertificate } from '../k8s_operations'
 import { ValidationError } from '../error'
-
-function hybridEncrypt(pubKey, plaintext, label) {
-  const sessionKey = crypto.randomBytes(32)
-  const cipher = crypto.createCipheriv('aes-256-gcm', sessionKey, Buffer.alloc(12, 0))
-  const encrypted = Buffer.concat([cipher.update(plaintext), cipher.final()])
-  const rsaEncryptedKey = crypto.publicEncrypt(
-    {
-      key: pubKey,
-      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-      oaepHash: 'sha256',
-      oaepLabel: label,
-    },
-    sessionKey,
-  )
-  const lenBuffer = Buffer.alloc(2)
-  lenBuffer.writeUInt16BE(rsaEncryptedKey.length, 0)
-  const finalCiphertext = Buffer.concat([lenBuffer, rsaEncryptedKey, encrypted, cipher.getAuthTag()])
-  return finalCiphertext.toString('base64')
-}
-
-function encryptionLabel(ns, secretName, scope) {
-  // Implement the logic to generate the label based on namespace, secret name, and scope
-  return `${ns}`
-}
-
-function getPublicKey(certificate) {
-  const x509 = new X509Certificate(certificate)
-  const value = x509.publicKey
-  return value.export({
-    format: 'pem',
-    type: 'spki',
-  })
-}
-
-export function encryptSecretItem(certificate, secretName, ns, data, scope) {
-  const pubKey = getPublicKey(certificate)
-  const label = encryptionLabel(ns, secretName, scope)
-  const out = hybridEncrypt(pubKey, data, label)
-  return out
-}
-
-export async function getEncryptedData(
-  decryptedData: Record<string, string | undefined> | undefined,
-  name: string,
-  namespace: string,
-): Promise<Record<string, string>> {
-  const certificate = await getSealedSecretsCertificate()
-  if (!certificate) {
-    const err = new ValidationError()
-    err.publicMessage = 'SealedSecrets certificate not found'
-    throw err
-  }
-  const encryptedDataPromises = Object.entries(decryptedData || {}).map(([key, value]) => {
-    if (value === undefined) {
-      return { [key]: value }
-    }
-    const encryptedItem = encryptSecretItem(certificate, name, namespace, value, 'namespace-wide')
-    return { [key]: encryptedItem }
-  })
-  return Object.assign({}, ...(await Promise.all(encryptedDataPromises || [])))
-}
+import { getSealedSecretsCertificate } from '../k8s_operations'
 
 export interface SealedSecretManifestType {
   apiVersion: string
@@ -126,4 +65,24 @@ export function sealedSecretManifest(data: AplSecretResponse): SealedSecretManif
       },
     },
   }
+}
+
+function getPEM(certificate): string {
+  const x509 = new X509Certificate(certificate)
+  const value = x509.publicKey
+  const exported = value.export({
+    format: 'pem',
+    type: 'spki',
+  })
+  return typeof exported === 'string' ? exported : exported.toString('utf-8')
+}
+
+export async function getSealedSecretsPEM(): Promise<string> {
+  const certificate = await getSealedSecretsCertificate()
+  if (!certificate) {
+    const err = new ValidationError()
+    err.publicMessage = 'SealedSecrets certificate not found'
+    throw err
+  }
+  return getPEM(certificate)
 }
