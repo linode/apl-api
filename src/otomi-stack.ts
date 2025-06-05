77893efc4913cc2f7bba60bef888f08e6bbfa44b
Sander Rodenhuis <srodenhu@akamai.com>
2025-06-05 14:13:38 +0200

    fix: bucket schema

--- src/otomi-stack.ts
+++ src/otomi-stack.ts
@@ -386,7 +386,7 @@ export default class OtomiStack {
         velero: `lke${lkeClusterId}-velero`,
         gitea: `lke${lkeClusterId}-gitea`,
         thanos: `lke${lkeClusterId}-thanos`,
-        kfp: `lke${lkeClusterId}-kfp`,
+        kubeflow-pipelines: `lke${lkeClusterId}-kubeflow-pipelines`,
       }
       const objectStorageClient = new ObjectStorageClient(data.apiToken)
       // Create object storage buckets
