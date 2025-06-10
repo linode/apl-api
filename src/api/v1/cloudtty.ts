45a9f0b7bde677240bb1c0b3f5b4da31dc5feda1
otomi-admin <63190600+ferruhcihan@users.noreply.github.com>
2025-06-10 17:05:36 +0200

    feat: update cloudtty shell endpoint and schema

--- src/api/v1/cloudtty.ts
+++ src/api/v1/cloudtty.ts
@@ -5,22 +5,23 @@ import { OpenApiRequestExt } from 'src/otomi-models'
 const debug = Debug('otomi:api:v1:cloudtty')
 
 export default function (): OperationHandlerArray {
-  const post: Operation = [
-    async ({ otomi, body }: OpenApiRequestExt, res): Promise<void> => {
-      debug(`connectCloudtty`)
-      const v = await otomi.connectCloudtty(body)
+  const get: Operation = [
+    async ({ otomi, query, user: sessionUser }: OpenApiRequestExt, res): Promise<void> => {
+      debug(`connectCloudtty - ${sessionUser.email} - ${sessionUser.sub}`)
+      const { teamId }: { teamId: string } = query as { teamId: string }
+      const v = await otomi.connectCloudtty(teamId, sessionUser)
       res.json(v)
     },
   ]
   const del: Operation = [
-    async ({ otomi, body }: OpenApiRequestExt, res): Promise<void> => {
-      debug(`deleteCloudtty`)
-      await otomi.deleteCloudtty(body)
+    async ({ otomi, user: sessionUser }: OpenApiRequestExt, res): Promise<void> => {
+      debug(`deleteCloudtty - ${sessionUser.email} - ${sessionUser.sub}`)
+      await otomi.deleteCloudtty(sessionUser)
       res.json({})
     },
   ]
   const api = {
-    post,
+    get,
     delete: del,
   }
   return api
