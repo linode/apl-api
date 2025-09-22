8c361d1d9d7b8a5a7a7a26a90df1c61592e76363
Cas Lubbers <clubbers@akamai.com>
2025-09-22 14:04:05 +0200

    feat: add authz tests

--- src/api/alpha/ai/models.ts
+++ src/api/alpha/ai/models.ts
@@ -7,9 +7,9 @@ const debug = Debug('otomi:api:alpha:ai:models')
 export default function (): OperationHandlerArray {
   const get: Operation = [
     /* business middleware not expressible by OpenAPI documentation goes here */
-    ({ otomi }: OpenApiRequestExt, res): void => {
+    async ({ otomi }: OpenApiRequestExt, res): Promise<void> => {
       debug('getAllAIModels')
-      const v = otomi.getAllAIModels()
+      const v = await otomi.getAllAIModels()
       res.json(v)
     },
   ]
