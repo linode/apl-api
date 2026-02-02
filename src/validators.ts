0ee5df25d198f9830c1bd02b9f12894ee5e4aff6
Cas Lubbers <clubbers@akamai.com>
2026-02-02 14:18:47 +0100

    feat: remove Minio

--- src/validators.ts
+++ src/validators.ts
@@ -75,7 +75,7 @@ export const TOOLS_HOST = str({ desc: 'The host of the tools server', default: '
 export const PREINSTALLED_EXCLUDED_APPS = json({
   desc: 'Applications that are managed by Linode, so they should be excluded from the apps page',
   default: {
-    apps: ['cert-manager', 'minio', 'external-dns'],
+    apps: ['cert-manager', 'external-dns'],
   },
 })
 export const HIDDEN_APPS = json({
