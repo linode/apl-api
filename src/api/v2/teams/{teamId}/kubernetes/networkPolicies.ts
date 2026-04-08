5f135cf3a2030e517cf74ee609eb2a2c67687bb0
dvankeke <dvankeke@akamai.com>
2026-04-08 10:28:19 +0200

    fix: kubernetes folder from v1 to v2

---
+++ src/api/v2/teams/{teamId}/kubernetes/networkPolicies.ts
@@ -0,0 +1,21 @@
+import Debug from 'debug'
+import { Response } from 'express'
+import { OpenApiRequestExt } from 'src/otomi-models'
+
+const debug = Debug('otomi:api:v2:teams:kubernetes:networkPolicies')
+
+/**
+ * GET /v2/teams/{teamId}/kubernetes/networkPolicies
+ * Get K8s pod labels for a workload
+ */
+export const getK8sPodLabelsForWorkload = async (req: OpenApiRequestExt, res: Response): Promise<void> => {
+  debug('getAllK8sPodLabelsForWorkload')
+  try {
+    const { workloadName, namespace } = req.query as { workloadName: string; namespace: string }
+    const v = await req.otomi.getK8sPodLabelsForWorkload(workloadName, namespace)
+    res.json(v)
+  } catch (e) {
+    debug(e)
+    res.json([])
+  }
+}
---
+++ src/api/v2/teams/{teamId}/kubernetes/services.ts
@@ -0,0 +1,21 @@
+import Debug from 'debug'
+import { Response } from 'express'
+import { OpenApiRequestExt } from 'src/otomi-models'
+
+const debug = Debug('otomi:api:v2:teams:kubernetes:services')
+
+/**
+ * GET /v2/teams/{teamId}/kubernetes/services
+ * Get all Kubernetes services for a team
+ */
+export const getK8sServices = async (req: OpenApiRequestExt, res: Response): Promise<void> => {
+  const { teamId } = req.params
+  debug('getAllK8sServices')
+  try {
+    const v = await req.otomi.getK8sServices(teamId)
+    res.json(v)
+  } catch (e) {
+    debug(e)
+    res.json([])
+  }
+}
