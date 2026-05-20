0f5d8eca68982115adfbcb9bf7eb41397fe5953f
dvankeke <dvankeke@akamai.com>
2026-05-20 14:39:47 +0200

    fix: acl for internalrepourls

--- src/api/v2/teams/{teamId}/internalRepoUrls.ts
+++ src/api/v2/teams/{teamId}/internalRepoUrls.ts
@@ -5,11 +5,14 @@ import { OpenApiRequestExt } from 'src/otomi-models'
 const debug = Debug('otomi:api:v2:internalRepoUrls')
 
 /**
- * GET /v2/internalRepoUrls
- * Get internal repository URLs
+ * GET /v2/teams/{teamId}/internalRepoUrls
+ * Get internal repository URLs for a team
  */
 export const getInternalRepoUrls = async (req: OpenApiRequestExt, res: Response): Promise<void> => {
-  debug(`getInternalRepoUrls ${req.query?.teamId}`)
-  const v = await req.otomi.getInternalRepoUrls(req.query?.teamId as string)
+  const { teamId } = req.params
+
+  debug(`getInternalRepoUrls ${teamId}`)
+
+  const v = await req.otomi.getInternalRepoUrls(teamId)
   res.json(v)
 }
