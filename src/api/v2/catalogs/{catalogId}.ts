d8ee4a15789889747c595946c5dd4fc6f3101838
jeho <17126497+j-zimnowoda@users.noreply.github.com>
2026-07-22 11:30:40 +0200

    feat: drop all additional properties from responses (#1030)
    Co-authored-by: svcAPLBot <174728082+svcAPLBot@users.noreply.github.com>

--- src/api/v2/catalogs/{catalogId}.ts
+++ src/api/v2/catalogs/{catalogId}.ts
@@ -1,5 +1,6 @@
 import Debug from 'debug'
 import { Response } from 'express'
+import { ensureStatus } from 'src/api/response-utils'
 import { AplCatalogRequest, OpenApiRequestExt } from 'src/otomi-models'
 
 const debug = Debug('otomi:api:v2:catalogs')
@@ -12,7 +13,7 @@ export const getAplCatalog = (req: OpenApiRequestExt, res: Response): void => {
   const { catalogId } = req.params
   debug(`getAplCatalog(${catalogId})`)
   const data = req.otomi.getAplCatalog(decodeURIComponent(catalogId))
-  res.json(data)
+  res.json(ensureStatus(data))
 }
 
 /**
@@ -23,7 +24,7 @@ export const editAplCatalog = async (req: OpenApiRequestExt, res: Response): Pro
   const { catalogId } = req.params
   debug(`editAplCatalog(${catalogId})`)
   const data = await req.otomi.editAplCatalog(decodeURIComponent(catalogId), req.body as AplCatalogRequest)
-  res.json(data)
+  res.json(ensureStatus(data))
 }
 
 /**
@@ -34,7 +35,7 @@ export const patchAplCatalog = async (req: OpenApiRequestExt, res: Response): Pr
   const { catalogId } = req.params
   debug(`patchAplCatalog(${catalogId})`)
   const data = await req.otomi.editAplCatalog(decodeURIComponent(catalogId), req.body as AplCatalogRequest, true)
-  res.json(data)
+  res.json(ensureStatus(data))
 }
 
 /**
@@ -45,5 +46,5 @@ export const deleteAplCatalog = async (req: OpenApiRequestExt, res: Response): P
   const { catalogId } = req.params
   debug(`deleteAplCatalog(${catalogId})`)
   await req.otomi.deleteAplCatalog(decodeURIComponent(catalogId))
-  res.json({})
+  res.status(200).end()
 }
